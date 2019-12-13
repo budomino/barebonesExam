b89c0ce5a44f40db3615864daffe1e46d6f40c61
bu.domino <bu.domino@gmail.com>
2019-12-13 18:37:03 +0800

    can add, delete, fetch, and update using address bar

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # barebonesExam
+
+**This is just code requested by an exam.** It is basically an example of a working CRUD app.
+
+##Restrictions are as follows:
+
+ - Library to use is only Mysql, EJS, and Express.
+
+ - Create a RESTful API that can return JSON or consume API call.
+
+ - Must be uploaded in Github with iterative commits.
+
+##Miscellaneous details (that are possibly important to reconstruct the app) from the developer of this app as follows:
+
+ - This was created in Arch Linux, running mysql57 from the AUR
+
+ - mysqld and other installation files from the package are untouched. Everything is installed in their default locations.
+
+ - IDE is Atom with several packages installed
+
+ - Most code is borrowed or based on the references below.
+
+##**Sources**
+
+
+ - https://medium.com/@bhanushali.mahesh3/creating-a-simple-website-with-node-js-express-and-ejs-view-engine-856382a4578f
+
+ - https://www.youtube.com/watch?v=EN6Dx22cPRI
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var mysql = require('mysql');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -13,6 +14,7 @@ const db = mysql.createConnection({
 	host	: 'localhost',
 	user 	: 'root',
 	password: '1234567890',
+	database: 'inventory'
 });
 
 //connect to mysql database
@@ -26,12 +28,78 @@ db.connect((err) => {
 var app = express();
 
 app.get('/createdb', (req, res) => {
-	let sql = 'CREATE DATABASE nodemysql';
+	let sql = 'CREATE DATABASE inventory';
 	db.query(sql, (err, result) => {
 		if(err) throw err;
 		res.send('Database created...');
 	});
 });
+
+//create table
+app.get('/createitemstable', (req, res) => {
+	let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+	db.query(sql,(err, result) => {
+		if(err) throw err;
+		console.log(result);
+		res.send('Posts table created...');
+	})
+})
+
+//fetch all posts
+app.get('/fetchallposts',(req, res) => {
+	let sql = 'SELECT * FROM posts';
+	let query = db.query(sql, (err, result) => {
+		if(err) throw err;
+		console.log(result);
+		res.send('Posts fetched...');
+
+	})
+})
+
+// insert post 1
+app.get('/addpost',(req, res) => {
+	let post = {title:'Post One', body:'This is post number one'};
+	let sql = 'INSERT INTO posts SET ?'
+	let query = db.query(sql, post, (err, result) => {
+		if(err) throw err;
+		console.log(result);
+		res.send('Posts 1 added...');
+
+	});
+});
+
+//get any post
+app.get('/getpost/:id', (req, res) => {
+	let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+	let query = db.query(sql, (err, result) => {
+		if(err) throw err;
+		console.log(result);
+		res.send('Post fetched');
+	});
+});
+
+// update post
+app.get('/updateitem/:id', (req, res) => {
+	let newTitle = 'Updated Title';
+	let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+	let query = db.query(sql, (err, result) => {
+		if(err) throw err;
+		console.log(result);
+		res.send('Post updated');
+	});
+});
+
+// delete post
+app.get('/deleteitem/:id', (req, res) => {
+	let newTitle = 'Updated Title';
+	let sql = `DELETE FROM posts WHERE id = ${req.params.id}`;
+	let query = db.query(sql, (err, result) => {
+		if(err) throw err; 
+		console.log(result);
+		res.send('Post deleted');
+	});
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
