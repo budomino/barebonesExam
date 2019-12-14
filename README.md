26a46c56d83c160e0c9d48c9c9dc652731bbe312
bu.domino <bu.domino@gmail.com>
2019-12-14 10:05:22 +0800

    I can view one row of data on page

--- README.md
+++ README.md
@@ -16,6 +16,8 @@
 
  - mysqld and other installation files from the package are untouched. Everything is installed in their default locations.
 
+ - mysql account is root@localhost with password "1234567890" (no quotes)
+
  - IDE is Atom with several packages installed
 
  - Most code is borrowed or based on the references below.
@@ -26,3 +28,9 @@
  - https://medium.com/@bhanushali.mahesh3/creating-a-simple-website-with-node-js-express-and-ejs-view-engine-856382a4578f
 
  - https://www.youtube.com/watch?v=EN6Dx22cPRI
+
+ - https://stackoverflow.com/questions/8829102/check-if-table-exists-without-using-select-from
+
+ - https://chartio.com/learn/databases/using-information-schema-views-to-check-to-see-if-table-exists-in-sql-server/
+
+ - https://stackoverflow.com/questions/51126022/displaying-mysql-query-results-as-html-with-ejs
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 //create connection to database
@@ -27,78 +27,10 @@ db.connect((err) => {
 
 var app = express();
 
-app.get('/createdb', (req, res) => {
-	let sql = 'CREATE DATABASE inventory';
-	db.query(sql, (err, result) => {
-		if(err) throw err;
-		res.send('Database created...');
-	});
-});
 
-//create table
-app.get('/createitemstable', (req, res) => {
-	let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
-	db.query(sql,(err, result) => {
-		if(err) throw err;
-		console.log(result);
-		res.send('Posts table created...');
-	})
-})
-
-//fetch all posts
-app.get('/fetchallposts',(req, res) => {
-	let sql = 'SELECT * FROM posts';
-	let query = db.query(sql, (err, result) => {
-		if(err) throw err;
-		console.log(result);
-		res.send('Posts fetched...');
-
-	})
-})
-
-// insert post 1
-app.get('/addpost',(req, res) => {
-	let post = {title:'Post One', body:'This is post number one'};
-	let sql = 'INSERT INTO posts SET ?'
-	let query = db.query(sql, post, (err, result) => {
-		if(err) throw err;
-		console.log(result);
-		res.send('Posts 1 added...');
-
-	});
-});
-
-//get any post
-app.get('/getpost/:id', (req, res) => {
-	let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
-	let query = db.query(sql, (err, result) => {
-		if(err) throw err;
-		console.log(result);
-		res.send('Post fetched');
-	});
-});
-
-// update post
-app.get('/updateitem/:id', (req, res) => {
-	let newTitle = 'Updated Title';
-	let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
-	let query = db.query(sql, (err, result) => {
-		if(err) throw err;
-		console.log(result);
-		res.send('Post updated');
-	});
-});
-
-// delete post
-app.get('/deleteitem/:id', (req, res) => {
-	let newTitle = 'Updated Title';
-	let sql = `DELETE FROM posts WHERE id = ${req.params.id}`;
-	let query = db.query(sql, (err, result) => {
-		if(err) throw err; 
-		console.log(result);
-		res.send('Post deleted');
-	});
-});
+//app.get('/', function(req, res) {
+//	res.render(__dirname + '/routes/index.js');
+//});
 
 
 // view engine setup
@@ -111,8 +43,105 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
+// CHECK IF TABLE AND DATABASE EXIST and then CREATE THEM
+//const existsTable = 'IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'horshe') SELECT 'found' AS search_result ELSE SELECT 'not found' AS search_result;';
+
+
+// create database
+app.get('/createdb', (req, res) => {
+	let sql = 'CREATE DATABASE IF NOT EXISTS inventory';
+	db.query(sql, (err, result) => {
+		if(err) res.send('Database already created');
+		console.log(result);
+		res.send('Database created...');
+	});
+});
+
+// create table
+app.get('/createitemstable', (req, res) => {
+//	if (!existsTable) {
+		let sql = "CREATE TABLE IF NOT EXISTS items (id int AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), quantity int UNSIGNED, amount DECIMAL(13,2))";
+		db.query(sql,(err, result) => {
+			if(err) throw err;
+			console.log('newly created database');
+			console.log(result);
+			res.send('items table created...');
+		});
+//	}
+//	console.log('previouslycreated');
+//	res.send('table already created');
+});
+
+// RETRIEVING
+
+//get all items
+var obj = {};
+app.get('/',(req, res) => {
+	let sql = 'SELECT * FROM items';
+	let query = db.query(sql, (err, result) => {
+		if(err) throw err;
+		//obj === {print: result};
+		console.log(result);
+		res.render('index', {data: result,});
+
+	})
+})
+
+//get any post
+app.get('/getitem/:id', (req, res) => {
+	let sql = `SELECT * FROM items WHERE id = ${req.params.id}`;
+	let query = db.query(sql, (err, result) => {
+		if(err) throw err;
+		console.log(result);
+		res.send(req.params.name);
+	});
+});
+
+
+// CREATING
+
+// insert post 1
+app.get('/additem',(req, res) => {
+	let post = {name:'testitem', quantity:'1', amount:'12.00'};
+	let sql = 'INSERT INTO items SET ?'
+	let query = db.query(sql, post, (err, result) => {
+		if(err) throw err;
+		console.log(result);
+		res.send('Post added...');
+
+	});
+});
+
+
+
+// update post
+app.get('/updateitem/:id', (req, res) => {
+	let newTitle = 'Updated Title';
+	let sql = `UPDATE items SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+	let query = db.query(sql, (err, result) => {
+		if(err) throw err;
+		console.log(result);
+		res.send('Post updated');
+	});
+});
+
+// delete post
+app.get('/deleteitem/:id', (req, res) => {
+	let newTitle = 'Updated Title';
+	let sql = `DELETE FROM items WHERE id = ${req.params.id}`;
+	let query = db.query(sql, (err, result) => {
+		if(err) throw err;
+		console.log(result);
+		res.send('Post deleted');
+	});
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -131,3 +160,7 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+// port environment check
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`Listening on port ${port}`));
